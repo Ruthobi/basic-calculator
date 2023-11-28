179c5271234fc631dda5f073e31211cccd7ab121
Ruthobi <prettychiomaobi9@gmail.com>
2023-11-28 09:55:46 +0100

    main

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Calculator1Component } from './calculator1/calculator1.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Calculator1Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/calculator1/calculator1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator1',
+  templateUrl: './calculator1.component.html',
+  styleUrls: ['./calculator1.component.css']
+})
+export class Calculator1Component {
+
+}
